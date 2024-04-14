00287bebddc39e254b6146b269ca84ed3c1ba265
shkwahab <wahab209266@gmail.com>
2024-04-14 20:08:41 +0500

    users screen

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -45,6 +45,57 @@ const Users = () => {
         </div>
       </div>
       <UsersTable />
+      <div id='PAGINATION' className='mt-10'>
+        <div className="flex items-center justify-between">
+          <div >
+            <span className="text-sm text-gray-700 ">
+              Showing data<span className=" ml-2 font-semibold ">1</span> to <span className="font-semibold ">10</span> of <span className="font-semibold  ">100</span> entries
+            </span>
+
+          </div>
+          <div>
+            <ul className="flex items-center space-x-4 h-10 text-base">
+              <li>
+                <a href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500  hover:text-gray-700 ">
+                  <span className="sr-only">Previous</span>
+                  <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center justify-center px-2 h-10 leading-tight text-white rounded-md bg-primary border  ">1</a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center justify-center px-2 h-10 leading-tight text-gray-500 rounded-md bg-white hover:bg-primary transition-all duration-300 hover:text-white">2</a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center justify-center px-2 h-10 leading-tight text-gray-500 rounded-md bg-white hover:bg-primary transition-all duration-300 hover:text-white">3</a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center justify-center px-2 h-10 leading-tight text-gray-500 rounded-md bg-white hover:bg-primary transition-all duration-300 hover:text-white">..</a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center justify-center px-2 h-10 leading-tight text-gray-500 rounded-md bg-white hover:bg-primary transition-all duration-300 hover:text-white">40</a>
+              </li>
+
+              <li>
+                <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 hover:text-gray-700">
+                  <span className="sr-only">Next</span>
+                  <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+
+
+
+        </div>
+
+      </div>
     </main>
   )
 }
