04be5718d19c3de0ba5790557f098727ce3da861
Asad00007 <asadsalim752@gmail.com>
2024-04-17 14:06:52 +0500

    Updated support, drivers, reports and sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,125 +1,176 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import { Link, useLocation } from 'react-router-dom'
-import logo from "../../assets/icons/Logo.svg"
-import hamBurger from "../../assets/icons/arrowleft.svg"
-import { Dashboard,Rides,Driver, Users, Reports, Support, Notifications, Settings } from "../SVGICONS/Icons"
+import React, { useState } from "react";
+import Navbar from "../Navbar/Navbar";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../../assets/icons/Logo.svg";
+import hamBurger from "../../assets/icons/arrowleft.svg";
+import {
+  Dashboard,
+  Rides,
+  Driver,
+  Users,
+  Reports,
+  Support,
+  Notifications,
+  Settings,
+} from "../SVGICONS/Icons";
 import { FaArrowRightLong } from "react-icons/fa6";
-import state from '../../proxyState/store'
-import { useSnapshot } from "valtio"
+import state from "../../proxyState/store";
+import { useSnapshot } from "valtio";
 const Sidebar = ({ children }) => {
-  const snap = useSnapshot(state)
+  const [showSideBar, setShowSideBar] = useState(true);
+  const snap = useSnapshot(state);
   const list = [
     {
       link: "/",
-      name: "Dashboard",
+      name: `${showSideBar ? "Dashboard" : ""}`,
       activeicon: <Dashboard color="#fff" size="30" />,
-      icon: <Dashboard color="#FFB000" size="30" />
+      icon: <Dashboard color="#FFB000" size="30" />,
     },
     {
       link: "/rides",
-      name: "Rides",
+      name: `${showSideBar ? "Rides" : ""}`,
       activeicon: <Rides color="#fff" size="30" />,
-      icon: <Rides color="#FFB000" size="30" />
+      icon: <Rides color="#FFB000" size="30" />,
     },
     {
       link: "/drivers",
-      name: "Drivers",
+      name: `${showSideBar ? "Drivers" : ""}`,
       activeicon: <Driver color="#fff" size="30" />,
-      icon: <Driver color="#FFB000" size="30" />
+      icon: <Driver color="#FFB000" size="30" />,
     },
     {
       link: "/users",
-      name: "Users",
+      name: `${showSideBar ? "Users" : ""}`,
       activeicon: <Users color="#fff" size="30" />,
-      icon: <Users color="#FFB000" size="30" />
+      icon: <Users color="#FFB000" size="30" />,
     },
     {
       link: "/reports",
-      name: "Reports",
+      name: `${showSideBar ? "Reports" : ""}`,
       activeicon: <Reports color="#fff" size="30" />,
-      icon: <Reports color="#FFB000" size="30" />
+      icon: <Reports color="#FFB000" size="30" />,
     },
     {
       link: "/support",
-      name: "Support",
+      name: `${showSideBar ? "Support" : ""}`,
       activeicon: <Support color="#fff" size="30" />,
-      icon: <Support color="#FFB000" size="30" />
+      icon: <Support color="#FFB000" size="30" />,
     },
     {
       link: "/notifications",
-      name: "Notifications",
+      name: `${showSideBar ? "Notifications" : ""}`,
       activeicon: <Notifications color="#fff" size="30" />,
-      icon: <Notifications color="#FFB000" size="30" />
+      icon: <Notifications color="#FFB000" size="30" />,
     },
     {
       link: "/settings",
-      name: "Settings",
+      name: `${showSideBar ? "Settings" : ""}`,
       activeicon: <Settings color="#fff" size="30" />,
-      icon: <Settings color="#FFB000" size="30" />
+      icon: <Settings color="#FFB000" size="30" />,
     },
+  ];
+  const path = useLocation().pathname;
 
-  ]
-  const path = useLocation().pathname
   return (
     <div>
-      <aside id="default-sidebar" className={`bg-[#FFB000]  ${snap.mobileSidebar === false ? " sidebarHide " : ""} lg:block fixed top-0 lg:left-0 left-0 z-40 w-64 h-screen  translate-x-0 transition-all duration-500`} aria-label="Sidebar">
-        <div id="scrollBar" className="h-full px-3 py-4 overflow-y-auto  ">
+      <aside
+        id="default-sidebar"
+        className={`bg-[#FFB000]  ${
+          snap.mobileSidebar === false ? " sidebarHide " : ""
+        } lg:block fixed top-0 lg:left-0 left-0 z-40 ${
+          !showSideBar ? "w-20" : "w-64"
+        } h-screen  translate-x-0 transition-all duration-500 `}
+        aria-label="Sidebar"
+      >
+        <div
+          className={`absolute  px-3 bg-transparent top-0 right-0 rounded-full ${
+            showSideBar ? "hidden" : ""
+          }`}
+        >
+          <div
+            onClick={() => setShowSideBar(!showSideBar)}
+            className={`bg-primary my-2 py-3 px-3 flex  rounded-full  justify-center ${
+              !showSideBar ? "translate-x-[100%]" : ""
+            } ${!showSideBar ? "translate-y-[60%]" : ""} `}
+          >
+            <img
+              src={hamBurger}
+              alt={"hamburger-Icn"}
+              className={`${!showSideBar ? "rotate-180 invert" : ""}`}
+            />
+            <img
+              src={hamBurger}
+              alt={"hamburger-Icn"}
+              className={`${!showSideBar ? "rotate-180 invert" : ""} `}
+            />
+          </div>
+        </div>
+        <div id="scrollBar" className="h-full px-3 py-4 overflow-y-auto ">
           <div className="flex items-center  justify-between">
-            <div className='flex items-center w-full flex-col '>
+            <div className="flex items-center w-full flex-col ">
               <div>
-                <img className=' sm:w-auto w-5/12' src={logo} alt="logo" />
+                <img className={`sm:w-auto`} src={logo} alt="logo" />
               </div>
-              <div className=' text-white text-2xl font-semibold'>
+              <div
+                className={`text-white text-2xl font-semibold ${
+                  !showSideBar ? "hidden" : ""
+                }`}
+              >
                 Uni Share
               </div>
             </div>
 
-            <div className='-mt-10 lg:block hidden'>
-              <div className='bg-white p-3 flex  rounded-full  justify-center'>
+            <div className="-mt-10 lg:block hidden">
+              <div
+                onClick={() => setShowSideBar(!showSideBar)}
+                className={`bg-white p-3 flex  rounded-full  justify-center  ${
+                  showSideBar ? "" : "hidden"
+                }`}
+              >
                 <img src={hamBurger} alt={"hamburger-Icn"} />
                 <img src={hamBurger} alt={"hamburger-Icn"} />
-
               </div>
             </div>
-            <div className='mr-2 lg:hidden' onClick={() => {
-              state.mobileSidebar = false
-            }}>
-              <FaArrowRightLong className='text-3xl  text-buttonPrimary' />
+            <div
+              className="mr-2 lg:hidden"
+              onClick={() => {
+                state.mobileSidebar = false;
+              }}
+            >
+              <FaArrowRightLong className="text-3xl  text-buttonPrimary" />
             </div>
           </div>
           <ul className="space-y-2 font-medium">
-            {
-              list.map((item, index) => {
-                return <Link key={index} to={item.link}>
-                  <li className={` font-medium font-primary flex text-xl my-8 items-center ${path === item.link ? " bg-white font-semibold text-primary rounded-lg py-3 px-2 items-center " : "text-[#fff]"}`}>
-                    <div className='mt-2 ml-2'>
-                      {path === item.link?item?.icon:item.activeicon}
+            {list.map((item, index) => {
+              return (
+                <Link key={index} to={item.link}>
+                  <li
+                    className={` font-medium font-primary flex text-xl my-8 items-center ${
+                      path === item.link
+                        ? " bg-white font-semibold text-primary rounded-lg py-3 px-2 items-center "
+                        : "text-[#fff]"
+                    }`}
+                  >
+                    <div className="mt-2 ml-2">
+                      {path === item.link ? item?.icon : item.activeicon}
                     </div>
-                    <div className=' ml-2'>
-                      {item.name}
-                    </div>
+                    <div className=" ml-2">{item.name}</div>
                   </li>
                 </Link>
-              })
-            }
-
-
+              );
+            })}
           </ul>
         </div>
       </aside>
 
       <div className="  lg:ml-64">
-        <div className=''>
+        <div className="">
           <Navbar />
         </div>
-        <div className="">
-          {children}
-        </div>
+        <div className="">{children}</div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
---
+++ src/components/SupportQuery/SupportQuery.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import profile from "../../assets/img/profile.jpeg";
+const SupportQuery = ({ setTrigger }) => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-40 z-50">
+      <div className="bg-white p-4 rounded-lg relative flex flex-col gap-3 w-[350px] md:max-w-[400px]">
+        <button
+          className="absolute top-4 right-4 text-black"
+          onClick={() => setTrigger(false)}
+        >
+          &#10006;
+        </button>
+        <h1 className="text-2xl font-semibold text-center">Support</h1>
+        <h1 className="text-primary font-semibold">Replying to Query by:</h1>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-3">
+              <img
+                src={profile}
+                alt=""
+                className="w-10 h-10 rounded-full object-cover bg-center"
+              />
+              <div className="flex flex-col">
+                <h2>Ablert Filma</h2>
+                <h2>September 9, 2023</h2>
+              </div>
+            </div>
+            <div>Query No. 121</div>
+          </div>
+          <p className="text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            facilis esse adipisci? Qui explicabo repudiandae libero expedita
+            dignissimos? Itaque accusamus quis perspiciatis inventore aut
+            veritatis esse optio error distinctio tempore.
+          </p>
+        </div>
+        <form action="" className="flex flex-col gap-3">
+          <label className="text-primary font-semibold">
+            Address the Query
+          </label>
+          <textarea
+            name="rejection"
+            id=""
+            cols="40"
+            rows="8"
+            className="border border-1 border-primary rounded-md outline-primary"
+          ></textarea>
+          <button className="px-3 py-2 bg-primary text-white rounded-md">
+            Reply
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SupportQuery;
